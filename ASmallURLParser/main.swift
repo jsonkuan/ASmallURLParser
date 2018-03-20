7b472f4d1bf947378ca653fdf9cced7825df0828
jsonkuan <jsonkuan@gmail.com>
2018-03-20 14:44:43 +0100

    Initial Commit

---
+++ ASmallURLParser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ASmallURLParser
+//
+//  Created by Jason Kuan on 2018-03-20.
+//  Copyright © 2018 Jason Kuan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
